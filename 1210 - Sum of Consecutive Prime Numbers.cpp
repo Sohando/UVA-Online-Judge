d3e74b35c50740715e714ea20616e4b15bd7a6c2
The_Dictator <sohandoabernahin1@gmail.com>
2018-05-01 18:45:18 +0600

    Update 1210 - Sum of Consecutive Prime Numbers.cpp

--- 1210 - Sum of Consecutive Prime Numbers.cpp
+++ 1210 - Sum of Consecutive Prime Numbers.cpp
@@ -3,59 +3,59 @@ using namespace std;
 
 vector<int> prime;
 int arr[10001];
-int counter[10001];
 
-void _baby() {
+void _baby()
+{
 
     prime.push_back(2);
 
-    for(int i=4;i<=10000;i+=2) arr[i] = 1;
+    for(int i=4; i<=10000; i+=2) arr[i] = 1;
 
-    for(int i=3;i<=10000;i+=2) {
-    	if(arr[i]==0) {
-    		prime.push_back(i);
-    		for(int j=i*i;j<=10000;j+=i+i) {
-    			arr[j] = 1;
-    		}
-    	}
-    }
-
-    for(int i=2;i<=10000;i++) {
-
-        int _nafsin = 0,_subna = 0;
-
-        if(!arr[i]) _nafsin++;
-
-        for(int j=0;prime[j]<=i/2;j++) {
-            _subna = 0;
-            for(int k=j;prime[k]<=i;k++) {
-
-                _subna += prime[k];
-
-                if(_subna==i)  {
-                    _nafsin++;
-                    break;
-                }
-                else if(_subna>i) break;
+    for(int i=3; i<=10000; i+=2)
+    {
+        if(arr[i]==0)
+        {
+            prime.push_back(i);
+            for(int j=i*i; j<=10000; j+=i+i)
+            {
+                arr[j] = 1;
             }
         }
-        counter[i] = _nafsin;
-
-        //cout << i << " " << counter[i] << endl;
     }
-
     return;
 }
 
-int main() {
+int main()
+{
 
-	_baby();
+    _baby();
 
-	int n;
+    int n;
 
-	while(scanf("%d",&n)==1,n) {
-        printf("%d\n",counter[n]);
-	}
+    while(scanf("%d",&n)==1,n)
+    {
+        int _nafsin = 0,_subna;
+        if(!arr[n]) _nafsin++;
 
-	return 0;
+        for(int j=0; prime[j]<=n/2; j++)
+        {
+            _subna = 0;
+            for(int k=j; prime[k]<=n; k++)
+            {
+
+                _subna += prime[k];
+
+                if(_subna==n)
+                {
+                    _nafsin++;
+                    break;
+                }
+                else if(_subna>n) break;
+            }
+        }
+
+        printf("%d\n",_nafsin);
+    }
+
+    return 0;
 }
