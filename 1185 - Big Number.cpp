1a012e884cf19e723985fe743a896bc975e32dd0
The_Dictator <sohandoabernahin1@gmail.com>
2018-05-05 03:12:34 +0600

    Create 1185 - Big Number.cpp

---
+++ 1185 - Big Number.cpp
@@ -0,0 +1,38 @@
+
+// A optimised program to find the
+// number of digits in a factorial
+#include <bits/stdc++.h>
+using namespace std;
+
+// Returns the number of digits present
+// in n! Since the result can be large
+// long long is used as return type
+long long findDigits(int n)
+{
+	// base case
+	if (n <= 1)
+		return 1;
+
+	// Use Kamenetsky formula to calculate
+	// the number of digits
+	double x = ((n * log10(n / M_E) +
+				log10(2 * M_PI * n) /
+				2.0));
+
+	return floor(x) + 1;
+
+
+}
+
+// Driver Code
+int main()
+{
+    int n,t;
+    scanf("%d",&n);
+
+    while(n--) {
+        scanf("%d",&t);
+        cout << findDigits(t) << endl;
+    }
+	return 0;
+}
