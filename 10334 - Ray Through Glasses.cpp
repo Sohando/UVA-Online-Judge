7ae8ffb87c5a22938ab3523df3ec0bc0322b766b
The_Dictator <sohandoabernahin1@gmail.com>
2018-08-14 12:58:04 +0600

    Create 10334 - Ray Through Glasses.cpp

---
+++ 10334 - Ray Through Glasses.cpp
@@ -0,0 +1,58 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+string fib,tib,tem;
+map< int, string > mp;
+
+string doSum(string a, string b)
+{
+    if(a.size() < b.size())
+        swap(a, b);
+
+    int j = a.size()-1;
+    for(int i=b.size()-1; i>=0; i--, j--)
+        a[j]+=(b[i]-'0');
+
+    for(int i=a.size()-1; i>0; i--)
+    {
+        if(a[i] > '9')
+        {
+            int d = a[i]-'0';
+            a[i-1] = ((a[i-1]-'0') + d/10) + '0';
+            a[i] = (d%10)+'0';
+        }
+    }
+    if(a[0] > '9')
+    {
+        string k;
+        k+=a[0];
+        a[0] = ((a[0]-'0')%10)+'0';
+        k[0] = ((k[0]-'0')/10)+'0';
+        a = k+a;
+    }
+    return a;
+}
+
+void fibonacci_string() {
+    
+    mp[0] = "1";
+    mp[1] = "2";
+    for( int i=2;i<=1001;i++ ) {
+        fib = mp[i-2];
+        tib = mp[i-1];
+        tem = doSum(fib,tib);
+        mp[i] = tem;
+        
+    }
+}
+int main() {
+
+    fibonacci_string();
+    int n;
+    while( scanf("%d",&n)==1) {
+
+        cout << mp[n] << endl;
+    }
+
+    return 0;
+}
