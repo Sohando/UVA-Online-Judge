db3467e1ac8a69b93f0f6a20d401c3e68eb0d62c
The_Dictator <sohandoabernahin1@gmail.com>
2018-10-07 02:23:02 +0600

    Create 11344 - The Huge One.cpp

---
+++ 11344 - The Huge One.cpp
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include<string.h>
+char Array[100000007];
+
+int bhagsesh(int val) {
+	int bs = 0;
+	int sz = strlen(Array);
+	int i;
+	
+	for(i=0; i<sz; i++) {
+		bs = ((bs*10) + Array[i]-'0') % val;
+	}
+	return bs;
+}
+
+int main(void) {
+	
+	int n;
+	scanf("%d", &n);
+	
+	while(n--) {
+		getchar();
+		scanf("%s", &Array);
+		
+		int t;
+		scanf("%d", &t);
+		
+		int foo = 0, m;
+		while(t--) {
+			scanf("%d", &m);
+			if(bhagsesh(m)) {
+				foo = 1;
+				
+			}
+		}
+		if(foo) {
+			printf("%s - Simple.\n", Array);
+		}
+		else {
+			printf("%s - Wonderful.\n", Array);
+		}
+	}
+	return 0;
+}
